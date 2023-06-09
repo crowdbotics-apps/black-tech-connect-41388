398d83a9302e51e3f19bcbbe068f7a018e36208f
Crowdbotics <team@crowdbotics.com>
2023-06-09 16:31:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "black-tech-connect-41388",
+  url: "https://black-tech-connect-41388.botics.co",
+  api: "https://black-tech-connect-41388.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
